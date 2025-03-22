2c9ea092484ca695276ab8c614a9e07231178872
Moisés Vieira Beltrame <moisess.vieiraa.beltramee@gmail.com>
2025-03-22 20:27:34 -0300

    fix

--- timer-worker.js
+++ timer-worker.js
@@ -1,50 +1,56 @@
 let timerStartTime = null;
 let timerDuration = 5; // Default duration in minutes
 let timeLeft = timerDuration * 60;
+let timerInterval = null;
 
 self.onmessage = function (e) {
     switch (e.data.type) {
         case 'START_TIMER':
-            timerStartTime = Date.now();
             timerDuration = e.data.duration;
             timeLeft = timerDuration * 60;
-            updateTimer();
+            timerStartTime = Date.now();
+            startTimer();
             break;
         case 'STOP_TIMER':
-            timerStartTime = null;
+            stopTimer();
             break;
         case 'UPDATE_DURATION':
             timerDuration = e.data.duration;
             timeLeft = timerDuration * 60;
+            if (timerInterval) {
+                stopTimer();
+                startTimer();
+            }
             break;
     }
 };
 
-function updateTimer() {
-    if (!timerStartTime) {
-        self.postMessage({
-            type: 'TIMER_UPDATE',
-            timeLeft: timeLeft,
-            isRunning: false
-        });
-        return;
+function startTimer() {
+    if (timerInterval) {
+        clearInterval(timerInterval);
     }
 
-    const currentTime = Date.now();
-    const elapsedTime = Math.floor((currentTime - timerStartTime) / 1000);
-    timeLeft = Math.max(0, (timerDuration * 60) - elapsedTime);
+    timerInterval = setInterval(() => {
+        const elapsed = Math.floor((Date.now() - timerStartTime) / 1000);
+        timeLeft = Math.max(0, (timerDuration * 60) - elapsed);
 
-    self.postMessage({
-        type: 'TIMER_UPDATE',
-        timeLeft: timeLeft,
-        isRunning: true
-    });
-
-    if (timeLeft > 0) {
-        setTimeout(updateTimer, 1000);
-    } else {
         self.postMessage({
-            type: 'TIMER_COMPLETE'
+            type: 'TIMER_UPDATE',
+            timeLeft: timeLeft
         });
+
+        if (timeLeft <= 0) {
+            stopTimer();
+            self.postMessage({
+                type: 'TIMER_COMPLETE'
+            });
+        }
+    }, 1000);
+}
+
+function stopTimer() {
+    if (timerInterval) {
+        clearInterval(timerInterval);
+        timerInterval = null;
     }
 } 
